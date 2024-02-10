708fac1eee67b69e40e2635b601f51e7b0032b34
Conkers1800 <diaz.1800xd@gmail.com>
2024-02-09 22:43:58 -0600

    Se modificaron las clases BlurWorker y WorkManagerBluromaticRepository para implementar segundo planocompletamente y modificacion a las notificaciones.

--- app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
+++ app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
@@ -4,8 +4,12 @@ import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import android.content.Context
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.util.Log
+import androidx.work.workDataOf
 import com.example.bluromatic.DELAY_TIME_MILLIS
+import com.example.bluromatic.KEY_BLUR_LEVEL
+import com.example.bluromatic.KEY_IMAGE_URI
 import com.example.bluromatic.R
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -13,25 +17,38 @@ import kotlinx.coroutines.withContext
 
 private const val TAG = "BlurWorker"
 class BlurWorker(ctx: Context, params: WorkerParameters) : CoroutineWorker(ctx, params) {
+    val resourceUri = inputData.getString(KEY_IMAGE_URI)
+    val blurLevel = inputData.getInt(KEY_BLUR_LEVEL, 1)
     override suspend fun doWork(): Result {
         makeStatusNotification(
             applicationContext.resources.getString(R.string.blurring_image),
             applicationContext
+
         )
+
+
         return withContext(Dispatchers.IO){
             delay(DELAY_TIME_MILLIS)
 
-           // val picture = BitmapFactory.decodeResource()
         return@withContext try{
-            val picture = BitmapFactory.decodeResource(
-                applicationContext.resources,
-                R.drawable.android_cupcake)
-                val output = blurBitmap(picture, 1)
+            require(!resourceUri.isNullOrBlank()) {
+                val errorMessage =
+                    applicationContext.resources.getString(R.string.invalid_input_uri)
+                Log.e(TAG, errorMessage)
+                errorMessage
+                }
+            val resolver = applicationContext.contentResolver
+
+            val picture = BitmapFactory.decodeStream(
+                resolver.openInputStream(Uri.parse(resourceUri))
+            )
+            //val picture = BitmapFactory.decodeResource(
+            //    applicationContext.resources,
+            //    R.drawable.android_cupcake)
+                val output = blurBitmap(picture, blurLevel)
                 val outputUri = writeBitmapToFile(applicationContext, output)
-            makeStatusNotification(
-                "Output is $outputUri",
-                applicationContext)
-            Result.success()
+            val outputData = workDataOf(KEY_IMAGE_URI to outputUri.toString())
+            Result.success(outputData)
         } catch (throwable: Throwable) {
             Log.e(
                 TAG,
